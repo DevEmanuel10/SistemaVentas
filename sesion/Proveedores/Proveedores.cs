270456bacfeb874cf710b9f20cb0a6fb950a598a
FernandoNeirot <fernando.neirot@softtek.com>
2020-04-26 09:42:57 -0300

    Proveedores, View

---
+++ sesion/Proveedores/Proveedores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sesion.Proveedores
+{
+    public partial class Proveedores : Form
+    {
+        public Proveedores()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
